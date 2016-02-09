e4eaa62aa36c2e3adaae52310b302aae2ce707b8
Daniel O'Malley <omalled@lanl.gov>
2016-02-09 14:15:27 -0700

    bugfix -- using memory twice; better testing

--- src/DistributionSums.jl
+++ src/DistributionSums.jl
@@ -14,23 +14,24 @@ function DistributionSum(numsamples::Int, bounds::Vector, dists...)
 	xs = linspace(bounds[1], bounds[2], numsamples)
 	pdfvals = Array(Float64, numsamples)
 	convdist = Float64[]
-	truncatedprobability = 0.
+	probability = 1.
 	for d in dists
-		truncatedprobability += 1 - (Distributions.cdf(d, bounds[2]) - Distributions.cdf(d, bounds[1]))
+		probability *= (Distributions.cdf(d, bounds[2]) - Distributions.cdf(d, bounds[1]))
 		map!(x->Distributions.pdf(d, x), pdfvals, xs)
 		if length(convdist) == 0
-			convdist = pdfvals
+			convdist = copy(pdfvals)
 		else
 			convdist = conv(pdfvals, convdist)
 		end
+	end
+	truncatedprobability = 1 - probability
+	if truncatedprobability > 0.001
+		warn("truncated probability is high: $truncatedprobability")
 	end
 	newbounds = length(dists) * bounds
 	normfactor = (sum(convdist) - .5 * (convdist[1] + convdist[end])) * (newbounds[2] - newbounds[1]) / (length(convdist) - 1)
 	scale!(convdist, 1 / normfactor)
 	range = newbounds[1]:(newbounds[2] - newbounds[1]) / (length(convdist) - 1):newbounds[2]
-	if truncatedprobability > 0.001
-		warn("truncated probability is high: $truncatedprobability")
-	end
 	DistributionSum(Grid.CoordInterpGrid(range, convdist, 0., Grid.InterpLinear), truncatedprobability)
 end
 
